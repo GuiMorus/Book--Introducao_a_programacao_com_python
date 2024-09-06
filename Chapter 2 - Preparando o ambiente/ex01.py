a2302b0c369bc047500287fd02113da6d54b54c9
Guilherme Moreira <gui.morushub@gmail.com>
2024-09-06 17:56:08 -0300

    Exercícios 1 e 2

---
+++ Chapter 2 - Preparando o ambiente/ex01.py
@@ -0,0 +1,13 @@
+# Converta as seguintes expressões matemáticas para que possam ser calculadas usando o
+# interpretador Python:
+# 10 + 20 x 30
+# 4² ÷ 30
+# (9⁴ + 2) x 6 - 1
+
+# Iniciando variáveis
+a = 10 + 20 * 30
+b = 4 ** 2 / 30
+c = (9 ** 4 + 2) * 6 - 1
+
+# Mostrando os resultados
+print(f"{a} \n{b} \n{c}")
