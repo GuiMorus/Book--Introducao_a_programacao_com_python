d5deda7091a050ac75675ed5e855578cf5d12ce3
Guilherme Moreira <gui.morushub@gmail.com>
2024-09-06 18:07:56 -0300

    Exercícios 03 ao 06

---
+++ Chapter 2 - Preparando o ambiente/ex04.py
@@ -0,0 +1,9 @@
+# Escreva um programa que exiba o resultado de 2a x 3b, em que a vale 3 e b vale 5
+
+# Iniciando variáveis
+a = 3
+b = 5
+formula = 2 * a * 3 * b
+
+# Mostrando resultado da formula
+print(formula)
---
+++ Chapter 2 - Preparando o ambiente/ex05.py
@@ -0,0 +1,10 @@
+# Escreva um programa que calcule a soma de três variáveis e imprima o resultado na tela
+
+# Iniciando variáveis
+a = 5
+b = 7
+c = -5
+resultado = a + b + c
+
+# Mostrando resultado
+print(resultado)
